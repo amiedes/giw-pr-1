84976cfa65e9aeb5b6286ece2c63f7516bf20711
dany reyes <danylop0423@outlook.com>
2016-10-22 15:28:23 +0200

    Terminado Ej 1.1...git status .. no se si he dejado bien el .gitignore

--- pr2/ej1/main.py
+++ pr2/ej1/main.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 """
 Created on Wed Oct 19 12:29:16 2016
 
@@ -6,11 +6,7 @@ Created on Wed Oct 19 12:29:16 2016
 """
 
 import csv
-import os
-
-
-
- 
+import os 
 """
 Ejercicio 1.1
 Usando el archivo agua_eprtr_2008_030412 crea un nuevo archivo csv denominado AguaAgrupada.cvs el cual debe contener agrupadas las empresas con el tipo de contaminación que producen. Tendrá la estructura:
@@ -22,36 +18,50 @@ def aguaAgrupada():
     os.chdir('..')
     os.chdir('emisiones_fuentes_contaminantes_csv_2008')
     src=open("agua_eprtr_2008_030412.csv")
+    
+    #reading file
     pointer=csv.reader(src,delimiter=";")
     content=list(pointer)
     final_content=[]
+ 
     for i in range(len(content)):
-        if i==1:
-            print "Contaminación,Empresa"
-        else:
-            final_content.append(content[i][8]+","+content[i][2])
+        if i!=0:
+            new_row=[]
+            new_row.append(content[i][8])
+            new_row.append(content[i][2])
+            final_content.append(new_row)        
     src.close()
     os.chdir("..")
+    
+    #making groups
     final_content.sort()
+    
+    # writing new file    
+    output=open("AguaAgrupada.csv","w")
+    w_pointer=csv.writer(output,lineterminator="\n")
     for n in range(len(final_content)):
-        #here i'm going to create a new file (not yet)
-        print final_content[n]
+        if n == 0:
+            w_pointer.writerow(["Contaminacion","empresa"])
+        else:
+            w_pointer.writerow (final_content[n])
+    output.close()        
     return        
-             
+    
+    
+     
     
 # Main Function to complete Exercise 1 
 def main():
-   while 1:
-       print " ----------------------"
-       print "| Gestor de Tareas CVS |"
-       print " ----------------------"
+   while True:
+       print " ----------------------"+"\n"+"| Gestor de Tareas CVS |"+"\n"+" ----------------------"
        print "Elige una Tarea:"
-       print "1.- Procesar Fichero Para Agrupar Por Contaminante"
+       print "1.- Generar Fichero --> 'AguaAgrupada.cvs' "
        print "2.- Titulo de Ej 1.2"
        print "3.- Titulo del Ej 1.3"
        opt=raw_input("INTRODUCE EL NUMERO DE TAREA  (para salir presiona 'q+Enter'):")
        if(opt=="1"):
             aguaAgrupada()
+            print "\n"+"FICHERO CREADO CORRECTAMENTE (en path: '../AguaAgrupada.csv')"
             break
        elif(opt=="2"):
             print "llamar func. ej 1.2 here"
