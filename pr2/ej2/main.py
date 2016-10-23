e47da41474d8fce79639acb577116267baea9920
Anna Pietrzak <195680@edu.p.lodz.pl>
2016-10-23 21:01:41 +0200

    Terminado ej2 a

--- pr2/ej2/main.py
+++ pr2/ej2/main.py
@@ -1,47 +1,58 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import division
 import json
 import os
 import re
+
 #from semaphore import Semaphore
 
-os.chdir('..')
-json_file = open('semaforos.json')
-os.chdir('ej2')
-
-data = json.load(json_file)
-semaphores_array = data['features']
-
-print "Semaphores array has " + str(len(semaphores_array)) + " elements"
-
-#ania: creo que deberiamos sacar informacion sobre la ubicacion de cada semaforo.. 
-#pero es que cada semaforo tiene distinto ubicacion pues no hay ningun semaforo 
-#con mayor frecuencia que los otros... o posiblemente no entendi bien el ejercicio
-semaphores_dictionary = {}
-for semaphore in semaphores_array:
+def findFrecuency():
+    os.chdir('..')
+    json_file = open('semaforos.json')
+    
+    data = json.load(json_file)
+    semaphores_array = data['features']
+    
+    semaphores_dictionary = {}
+    totalSemaphoreNumber = 0
+    for semaphore in semaphores_array:
+        
         semaphoreDescription = semaphore['properties']['Description']
-        ubicacion = re.search('n:</b>(.+?)<br />', semaphoreDescription) #try to find a substring with ubicacion in string with description
+        ubicacion = re.search('n:</b> (.+?)<br />', semaphoreDescription) #try to find a substring with ubicacion in string with description
         if ubicacion:
             foundUbicacion = ubicacion.group(1)
-        print foundUbicacion
-
-"""alberto
-for semaphore in semaphores_array:
-    s = Semaphore(semaphore['geometry']['coordinates'][0], semaphore['geometry']['coordinates'][1])
-    #coordinates_tuple = (semaphore['geometry']['coordinates'][0], semaphore['geometry']['coordinates'][1])
-    #print coordinates_tuple
-    if (s in semaphores_dictionary):
-        semaphores_dictionary[s] += 1
-    else:
-        semaphores_dictionary[s] = 1
-    #print "-"*30 + "\n"
-
-print "\n\n" + "=" * 40 + "\n\n"
-
-for key in semaphores_dictionary:
-    print key
-    print "\t" + str(semaphores_dictionary[key])
-
-print "Semaphores dictionary has " + str(len(semaphores_dictionary.keys())) + " elements."
-
-alberto"""
+            
+        num_semaforos = re.search('foros:</b> (.+?)<br />', semaphoreDescription) #try to find a substring with number of semaphores in string with description
+        if num_semaforos:
+            found_num_semaforos = num_semaforos.group(1)
+        semaphores_dictionary[foundUbicacion]=found_num_semaforos #dictionary with ubicacion as key and corresponding number of semaphores as value
+    
+        totalSemaphoreNumber = totalSemaphoreNumber + int(found_num_semaforos)
+    
+    #change number of semaphores as value into frecuency
+    for ubicacion, numOfSemaphores in semaphores_dictionary.items():
+        frecuency = int(numOfSemaphores) / float(totalSemaphoreNumber)
+        semaphores_dictionary[ubicacion] = frecuency
+    
+        
+    with open('SemaforosFrecuencia.json', 'w') as json_file:
+        json.dump(semaphores_dictionary, json_file)
+    json_file.close()
+    
+def main():
+     while True:
+       print "Elige una Tarea:"
+       print "1.- Encontrar la frecuencia de cada semaforo "
+       print "2.- Encontrar los 10 semáforos que aparecen con mayor frecuencia "
+       opt=raw_input("INTRODUCE EL NUMERO DE TAREA  (para salir presiona 'q+Enter'):")
+       if(opt=="1"):
+            findFrecuency()
+            print "\n"+"FICHERO CREADO CORRECTAMENTE (en path: '../SemaforosFrecuencia.json')"
+            break
+       elif(opt=="2"):
+            
+            break
+       elif(opt=="q"):
+            break
+main()
