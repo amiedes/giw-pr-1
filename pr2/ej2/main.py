276ec8b0d9d4870daa5fbcd593c1dea0a14dda12
Anna Pietrzak <195680@edu.p.lodz.pl>
2016-10-26 16:03:21 +0200

    casi terminado 2... queda escribir salida a .txt

--- pr2/ej2/main.py
+++ pr2/ej2/main.py
@@ -6,17 +6,18 @@ from __future__ import division
 import json
 import os
 import re
+import operator
 
 #from semaphore import Semaphore
 
-def findFrecuency():
+def findDictionary():
     os.chdir('..')
     json_file = open('semaforos.json')
     
     data = json.load(json_file)
     semaphores_array = data['features']
     
-    semaphores_dictionary = {}
+    semaphores_dictionary = dict()
     totalSemaphoreNumber = 0
     for semaphore in semaphores_array:
         
@@ -37,11 +38,35 @@ def findFrecuency():
         frecuency = int(numOfSemaphores) / float(totalSemaphoreNumber)
         semaphores_dictionary[ubicacion] = frecuency
     
-        
+    return semaphores_dictionary
+    
+def findFrecuency():
+    semaphores_dictionary = dict()
+    semaphores_dictionary = findDictionary()
     with open('SemaforosFrecuencia.json', 'w') as json_file:
         json.dump(semaphores_dictionary, json_file)
     json_file.close()
+    print "\n"+"FICHERO CREADO CORRECTAMENTE (en path: '../SemaforosFrecuencia.json')"
     
+def find10Frecuencies():
+    semaphores_dictionary = dict()
+    semaphores_dictionary = findDictionary()
+    for ubicacion, frecuencia in semaphores_dictionary.items():
+        semaphores_dictionary[ubicacion] = frecuencia
+    orderedDict = sorted(semaphores_dictionary.items(), key=operator.itemgetter(1), reverse=True)
+    
+    #print orderedDict
+    tamano = len(orderedDict)
+    first10 = tuple()
+    for n in range(10):
+        first10[orderedDict[tamano-(n+1)][0]] = orderedDict[tamano-(n+1)][1]
+    
+    #print first10
+    with open('FrecuenciaOrdenada.json', 'w') as json_file:
+        json.dump(orderedDict, json_file)
+    json_file.close()
+    print "\n"+"FICHERO CREADO CORRECTAMENTE (en path: '../FrecuenciaOrdenada.json')"    
+        
 def main():
      while True:
        print "Elige una Tarea:"
@@ -50,10 +75,9 @@ def main():
        opt=raw_input("INTRODUCE EL NUMERO DE TAREA  (para salir presiona 'q+Enter'):")
        if(opt=="1"):
             findFrecuency()
-            print "\n"+"FICHERO CREADO CORRECTAMENTE (en path: '../SemaforosFrecuencia.json')"
             break
        elif(opt=="2"):
-            
+            find10Frecuencies()
             break
        elif(opt=="q"):
             break
