f8c0287639feaf31dfca824f238ec1e7528e3c58
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-10-31 18:50:45 +0100

    XML parsing seems working

--- pr3-xml/main.py
+++ pr3-xml/main.py
@@ -4,16 +4,38 @@
 @authors: Daniel Reyes, Ania Pietrzak, Alberto Miedes
 """
 
+from xml.etree import ElementTree
+from monument import Monument
+
 # parses XML file into a list of 'monument' objects
-def parse_xml(filename):
+def parse_xml():
     monuments = []
-    print "Parsing XML file..."
-    # TODO: parse XML file
+    fh = open("MonumentosZaragoza.xml", "rt")
+    tree = ElementTree.parse(fh)
+
+    for punto_interes in tree.iterfind("Feature"):
+
+        for info in punto_interes:
+            field_name = info.get("name")
+            if   field_name == "nombre":
+                nombre = info.text
+            elif field_name == "url":
+                url = info.text
+            elif field_name == "posicion_x":
+                posicion_x = info.text
+            elif field_name == "posicion_y":
+                posicion_y = info.text
+        monument = Monument(nombre, url, posicion_x, posicion_y)
+
+        print "-"*20
+        print monument
+        print "-"*20
 
     return monuments
 
 # returns True if string is a valid monument ID, otherwise returns False
 def valid_id(s):
+    if (s == None): return False
     try:
         int(s)
         return True
@@ -23,16 +45,15 @@ def valid_id(s):
 def main():
     print "Booting up..."
 
-    monuments = parse_xml("MonumentosZaragoza.xml")
+    monuments = parse_xml()
     print monuments
 
     exit = False
-
-    while(!exit):
+    while(not exit):
         # ask for monument in the list
         monument_id = None
-        while(!valid_id(monument_id)):
-            monument_id = raw_input("Elige un monumento (ID):"))
+        while(not valid_id(monument_id)):
+            monument_id = raw_input("Elige un monumento (ID):")
 
         # get monument info and print
         monument = monuments[monument_id]
