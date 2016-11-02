97fca8ac0a1e0570459bc81ca381716366e48c97
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-11-02 18:20:07 +0100

    Use regex to find monument description inside the XML file

--- pr3-xml/monument.py
+++ pr3-xml/monument.py
@@ -5,6 +5,7 @@
 """
 
 import urllib
+import re
 import xml.etree.ElementTree as ET
 from html_tag_cleaner import HtmlTagCleaner
 
@@ -55,24 +56,26 @@ class Monument:
             # make API call
             web_content = urllib.urlopen(self.website)
             data = web_content.read().decode('latin-1')
-            # find the description inside all the response
-            initial_pos = data.find("<h3>Descripc")
-            final_pos = data.find("<h3>Enlaces</h3>")
-            description = data[initial_pos:final_pos]
-            # delete HTML tags
-            final_description = self.parse_description(description)
-            # set object description
-            self.description = final_description
+            # find the description inside the response
+            matches = re.search('\<h3\>Descripci.n\<\/h3\>(.|\n)*\<h3\>Enlaces\<\/h3\>', data)
+            if matches == None:
+                self.description = '(!!) Website description did not match standard structure'
+            else:
+                dirty_description = matches.group(0)
+                self.description = HtmlTagCleaner.clean(dirty_description)
 
     def set_geolocation(self):
         if (self.latitude is None) or (self.longitude is None):
+            # prepare API call
             serviceurl = 'http://maps.googleapis.com/maps/api/geocode/xml?'
             url = serviceurl + urllib.urlencode({
                 'address': self.name.encode('utf-8'),
                 'components': 'country:ES|administrative_area:Zaragoza'
             })
+            # make API call and retrieve data
             uh = urllib.urlopen(url)
             data = uh.read()
+            # extract latitude and longitude from XML response
             root = ET.fromstring(data)
             location = root.find('result').find('geometry').find('location')
             self.latitude = location.find('lat').text
