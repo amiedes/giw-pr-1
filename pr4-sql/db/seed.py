4a4a47b81ea720728a98083bd52c3f7a486609b8
dany reyes <danylop0423@outlook.com>
2016-11-05 23:49:42 +0100

    help me

--- pr4-sql/db/seed.py
+++ pr4-sql/db/seed.py
@@ -3,12 +3,57 @@
 """
 @authors: Daniel Reyes, Ania Pietrzak, Alberto Miedes
 """
+import sqlite3
+import csv
 
+def encode(text):
+    return text.encode('utf-8')
+
+def build_insert_query(content,table):
+    columns=content[0]
+    marks=list()
+    for i in range(len(columns)):
+        marks.append("?")    
+    separator=","
+    marks_str=separator.join(marks)
+    marks_str=" (" + marks_str + ")"
+    columns_str=separator.join(columns)
+    query="INSERT INTO "+table+" (" +columns_str+") Values"+marks_str
+    print query
+    return query
+
+    
+def execute_insert_query(content,query):
+    rows=list()
+    print content    
+    for i in range(len(content)):
+        if i==0:
+            continue
+        else:
+            single_row=tuple(content[i])
+            rows.append(single_row)
+    #print rows  #esto lo he dejado solo por ver la codificacion      
+    
+    conn = sqlite3.connect('Libreria')
+    cur = conn.cursor()
+    cur.executemany(query,rows)
+    msg=cur.rowcount
+    print msg , "filas insertadas correctamente!!"
+    cur.close()
+    conn.commit()
+    conn.close()
+    
 def seed_books_table():
-    print "seed_customers_table()"
     # TODO: parse db/seeds/libros.csv and insert its records into DB
-
-
+    src=open("db/seeds/libros.csv") 
+    pointer=csv.reader(src)
+    content=list(pointer)
+    table="Libros"
+    query=build_insert_query(content,table)
+    execute_insert_query(content,query)
+    src.close()    
+        
+        
 def seed_customers_table():
     print "seed_customers_table()"
     # TODO: parse db/seeds/compradores.csv and insert its records into DB
