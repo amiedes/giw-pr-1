3dbf06dd287e99d90838340a1e9ffa788698c917
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-11-12 12:01:01 +0100

    Search is now done with a form instead of URL params

--- pr6-web/controllers/consulate_controller.py
+++ pr6-web/controllers/consulate_controller.py
@@ -1,9 +1,16 @@
-from bottle import route, template
+from bottle import route, template, request
 from models.consulate import Consulate
 
 
-@route('/consulates/<filter_name>/<filter_value>')
-def filter(filter_name, filter_value):
+@route('/consulates/filter')
+def filter_form():
+    return template('consulates_filter_form.tpl')
+
+
+@route('/consulates/filter', method='POST')
+def filter_results():
+    filter_name = request.forms.get('field_name')
+    filter_value = request.forms.get('field_value')
     consulates = Consulate.find(filter_name, filter_value)
     return template('filtered_consulates.tpl', consulates=consulates)
 
---
+++ pr6-web/views/consulates_filter_form.tpl
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lant="es">
+<head></head>
+<body>
+  Specify a search filter:
+  <form action="filter" method="post">
+    Field name: <input name="field_name" type="text"/>
+    Field value: <input name="field_value" type="text"/>
+    <input value="Search" type="submit"/>
+  </form>
+</body>
+</html>
