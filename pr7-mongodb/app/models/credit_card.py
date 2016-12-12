c3f39c17ae6d560a287389fdf84b6140aa33bffd
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-12-12 21:24:35 +0100

    Added headers

--- pr7-mongodb/app/models/credit_card.py
+++ pr7-mongodb/app/models/credit_card.py
@@ -1,7 +1,15 @@
 #-*- coding: utf-8 -*-
 
 """
-@authors: Daniel Reyes, Ania Pietrzak, Alberto Miedes
+Autores: Daniel Reyes, Ania Pietrzak, Alberto Miedes
+Asignatura: Gestión de Información en la Web (GIW) - Práctica 7 - Grupo 1
+
+Daniel Reyes, Ania Pietrzak y Alberto Miedes declaramos que esta solución es
+fruto exclusivamente de nuestro trabajo personal. No hemos sido ayudados por
+ninguna otra persona ni hemos obtenido la solución de fuentes externas, y
+tampoco hemos compartido nuestra solución con nadie. Declaramos además que no
+hemos realizado de manera deshonesta ninguna otra actividad que pueda mejorar
+nuestros resultados ni perjudicar los resultados de los demás.
 """
 
 class CreditCard:
--- pr7-mongodb/consultas.py
+++ pr7-mongodb/consultas.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
-##
-## INCLUIR LA CABECERA AQUI
-##
+"""
+Autores: Daniel Reyes, Ania Pietrzak, Alberto Miedes
+Asignatura: Gestión de Información en la Web (GIW) - Práctica 7 - Grupo 1
+
+Daniel Reyes, Ania Pietrzak y Alberto Miedes declaramos que esta solución es
+fruto exclusivamente de nuestro trabajo personal. No hemos sido ayudados por
+ninguna otra persona ni hemos obtenido la solución de fuentes externas, y
+tampoco hemos compartido nuestra solución con nadie. Declaramos además que no
+hemos realizado de manera deshonesta ninguna otra actividad que pueda mejorar
+nuestros resultados ni perjudicar los resultados de los demás.
+"""
 
 from bottle import *
 from pymongo import *
