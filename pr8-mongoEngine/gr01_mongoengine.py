4f564b335e08c68c98a2aff0d2356abf84692ac9
danylop0423 <danylop0423@outlook.com>
2016-12-14 15:42:16 +0100

    class usuario defined

--- pr8-mongoEngine/gr01_mongoengine.py
+++ pr8-mongoEngine/gr01_mongoengine.py
@@ -5,12 +5,25 @@ Created on Wed Dec 14 14:38:00 2016
 
 @author: dany
 """
-from mongoengine import Connection, connect
+from mongoengine import connect,Document,StringField,ComplexDateTimeField\
+                        ,ListField,EmbeddedDocumentField,ReferenceField\
+                        ,CASCADE
 
-connection=Connection()
-connection.drop_database('giw mongoengine')
-#connection.close()
+connect('giw mongoengine')
 
-db=connection['giw mongoengine']
-db=connect('giw mongoengine')
+class Tarjeta_Credito(Document):
+ lop=StringField(db_field='dni',primary_key=True)# only to test
 
+class Pedido(Document):
+ lop=StringField(db_field='dni',primary_key=True)# only to test
+
+class Usuario(Document):
+    DNI = StringField(db_field='dni',primary_key=True)
+    nombre = StringField(required=True)
+    primer_apellido=StringField(required=True)
+    segundo_apellido=StringField()
+    fecha_nacimiento=StringField(required=True)
+    fecha_ultimo_acceso=ComplexDateTimeField()
+    tarjetas_credito=ListField(EmbeddedDocumentField(Tarjeta_Credito))
+    pedidos=ListField(ReferenceField(Pedido,reverse_delete_rule=CASCADE))
+    
