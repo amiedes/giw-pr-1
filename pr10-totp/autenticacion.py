7995685c99f2033d5a4d77c2d3bcfccc645bbdb6
Alberto Miedes Garcés <albertomg994@gmail.com>
2017-01-15 12:17:24 +0100

    Initial commit for pr-10

---
+++ pr10-totp/autenticacion.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+#
+# CABECERA AQUI
+#
+
+
+from bottle import run, post
+# Resto de importaciones
+
+
+##############
+# APARTADO 1 #
+##############
+
+# 
+# Explicación detallada del mecanismo escogido para el almacenamiento de c
+# contraseñas, explicando razonadamente por qué es seguro
+#
+
+
+@post('/signup')
+def signup():
+    pass
+    
+
+@post('/change_password')
+def change_password():
+    pass
+            
+
+@post('/login')
+def login():
+    pass
+
+
+##############
+# APARTADO 2 #
+##############
+
+
+def gen_secret():
+    # >>> gen_secret()
+    # '7ZVVBSKR22ATNU26'
+    pass
+    
+    
+def gen_gauth_url(app_name, username, secret):
+    # >>> gen_gauth_url( 'GIW_grupoX', 'pepe_lopez', 'JBSWY3DPEHPK3PXP')
+    # 'otpauth://totp/pepe_lopez?secret=JBSWY3DPEHPK3PXP&issuer=GIW_grupoX
+    pass
+        
+
+def gen_qrcode_url(gauth_url):
+    # >>> gen_qrcode_url('otpauth://totp/pepe_lopez?secret=JBSWY3DPEHPK3PXP&issuer=GIW_grupoX')
+    # 'http://api.qrserver.com/v1/create-qr-code/?data=otpauth%3A%2F%2Ftotp%2Fpepe_lopez%3Fsecret%3DJBSWY3DPEHPK3PXP%26issuer%3DGIW_grupoX'
+    pass
+    
+
+
+@post('/signup_totp')
+def signup_totp():
+    pass
+        
+        
+@post('/login_totp')        
+def login_totp():
+    pass
+
+    
+if __name__ == "__main__":
+    # NO MODIFICAR LOS PARÁMETROS DE run()
+    run(host='localhost',port=8080,debug=True)
